d6a5f3f2a1261b43696e28a99f3e5f2b977b265d
Laser Nite <lasernite@gmail.com>
2015-04-21 23:49:16 -0400

    redirect www to non-www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :redirect_subdomain
+
+  def redirect_subdomain
+  	if request.host == 'www.local.network'
+    	redirect_to 'http://local.network' + request.fullpath
+  	end
+  end
+
 end

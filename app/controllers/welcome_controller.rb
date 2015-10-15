62378f9c32db56e797b06e6076be882f256222c3
Laser Nite <lasernite@gmail.com>
2015-10-15 00:35:27 -0400

    some updates, like handling city specification in controller to eliminate errors, darkening boxes, and apparently email confirmation is implemented too, but needs styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def index
   	@user = User.new
+
+  	# Get city name
+  	@city = request.location.city
+  	if @city == nil or @city == ''
+  		@city = ''
+  	end
+
   end
 end

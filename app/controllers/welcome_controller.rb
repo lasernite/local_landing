c4460cd41113a4fe4c5231dc35a89dd2c3f51d06
Laser Nite <lasernite@gmail.com>
2015-10-21 21:37:59 -0400

    city geocoder bug should be fixed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,13 @@ class WelcomeController < ApplicationController
   def index
   	@user = User.new
 
-  	# Get city name
-  	@city = request.location.city
-  	if @city == nil or @city == ''
+  	# Get city name, overcome slow geocoder bug so doesn't crash whole page
+  	@city = ''
+  	@is_city = request.location.city
+  	if @is_city == nil or @is_city == ''
   		@city = ''
+  	else
+  		@city = @is_city
   	end
 
   end

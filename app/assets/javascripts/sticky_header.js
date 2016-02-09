d9512015cc39470aacebc348ba56a59a6aa4aba2
Laser Nite <lasernite@gmail.com>
2016-02-09 01:23:36 -0500

    fourth section mostly there love ;)

--- app/assets/javascripts/sticky_header.js
+++ app/assets/javascripts/sticky_header.js
@@ -1,21 +1,30 @@
 // Sticky Top Bar on Scroll Down
 
-var topNavResize = function() {
+var topNavEffect = function() {
 	var  top_nav = $(".top_nav");
     top_nav_scrolled = "top_nav_scrolled";
+    top_nav_color_1 = "top_nav_color_1"
     section1 = $('.section1').height();
-    main_text = $('.main_text').height();
+    section2 = $('.section2').height();
+    section3 = $('.section3').height();
 
 	$(window).scroll(function() {
-	  if( $(this).scrollTop() > section1 - 1 ) {
+	  if( $(this).scrollTop() > section1 - 1 && $(this).scrollTop() < section1 + section2 + section3 - 54 ) {
 	    top_nav.addClass(top_nav_scrolled);
-	  } else {
+	    top_nav.addClass(top_nav_color_1);
+	  } 
+	  else if ( $(this).scrollTop() >= section1 + section2 + section3 - 54 ) {
+	  	top_nav.addClass(top_nav_scrolled);
+	  	top_nav.removeClass(top_nav_color_1);
+	  }
+	  else {
 	    top_nav.removeClass(top_nav_scrolled);
+	    top_nav.removeClass(top_nav_color_1);
 	  }
 	});
 }
 
-$(document).ready(topNavResize);
-$(window).resize(topNavResize);
+$(document).ready(topNavEffect);
+$(window).resize(topNavEffect);
 
 

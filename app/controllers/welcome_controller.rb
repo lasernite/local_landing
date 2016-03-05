ee657d694491c762b0ec5d52dc8af1eedff6bb58
Laser Nite <lasernite@gmail.com>
2016-03-05 17:10:58 -0500

    removed city dependence, need to fix geocoder as the freegeoip api has become unreliable

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,15 @@ class WelcomeController < ApplicationController
   def index
   	@user = User.new
 
-  	# Get city name, overcome slow geocoder bug so doesn't crash whole page
-  	@city = ''
-  	@is_city = request.location.city
-  	if @is_city == nil or @is_city == ''
-  		@city = ''
-  	else
-  		@city = @is_city
-  	end
+  	# # Get city name, overcome slow geocoder bug so doesn't crash whole page
+  	# @city = ''
+  	# @is_city = request.location.city
+  	# if @is_city == nil or @is_city == ''
+  	# 	@city = ''
+  	# else
+  	# 	@city = @is_city
+  	# end
+    @city = "turned off"
 
   end
 end
